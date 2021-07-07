dc6af192696139f9c5f960f73f263e80aeb76a1f
Laurent Payot <laurent.payot@publiberty.com>
2021-07-07 17:20:30 +0200

    google sign in

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,8 @@ export default {
   input: 'src/main.js',
   output: {
     file: 'dist/main.js',
-    format: 'iife'
+    format: 'iife',
+    name: 'sscceFirebase'
   },
   plugins: [
     resolve(),
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, useAuthEmulator, onAuthStateChanged } from 'firebase/auth'
+import {
+    getAuth, useAuthEmulator, onAuthStateChanged,
+    signInWithRedirect, OAuthProvider
+} from 'firebase/auth'
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDcv23_D6Li-1lD04WLiuu7M4tTkHEsPfQ",
@@ -14,8 +18,19 @@ initializeApp(firebaseConfig)
 const auth = getAuth()
 if (location.hostname === "localhost") useAuthEmulator(auth, "http://localhost:9099")
 
+export function signInWithProvider(providerId) {
+    const provider = new OAuthProvider(providerId)
+    provider.addScope('profile')
+    provider.addScope('email')
+    return signInWithRedirect(auth, provider)
+}
+
 onAuthStateChanged(auth, user => {
-    const element = document.body.querySelector("#current-user-email")
-    element.innerHTML = user?.email
+    const emailEl = document.body.querySelector("#current-user-email")
+    emailEl.innerHTML = user?.email
 })
 
+window.onload = () => {
+    const signInEl = document.body.querySelector("#google-sign-in")
+    signInEl.addEventListener('click', () => signInWithProvider('google.com'))
+}
