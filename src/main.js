eaae24849d8f39583d7d32baaa5452cec7228ba8
Laurent Payot <laurent.payot@publiberty.com>
2021-07-08 11:23:43 +0200

    es module format

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import {
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcv23_D6Li-1lD04WLiuu7M4tTkHEsPfQ",
-  authDomain: "sscce-firebase.firebaseapp.com",
-  projectId: "sscce-firebase",
-  storageBucket: "sscce-firebase.appspot.com",
-  messagingSenderId: "963064759534",
-  appId: "1:963064759534:web:5a32477e5b4a63aff91fff"
+    apiKey: "AIzaSyDcv23_D6Li-1lD04WLiuu7M4tTkHEsPfQ",
+    authDomain: "sscce-firebase.firebaseapp.com",
+    projectId: "sscce-firebase",
+    storageBucket: "sscce-firebase.appspot.com",
+    messagingSenderId: "963064759534",
+    appId: "1:963064759534:web:5a32477e5b4a63aff91fff"
 }
 
 initializeApp(firebaseConfig)
@@ -30,13 +30,12 @@ onAuthStateChanged(auth, user => {
     emailEl.innerHTML = user?.email
 })
 
-window.onload = async () => {
-  const redirectResult = await getRedirectResult(auth)
-  if (redirectResult) {
+
+const redirectResult = await getRedirectResult(auth)
+if (redirectResult) {
     const user = redirectResult.user
     console.log("user =", user)
-  }
-
-  const signInEl = document.body.querySelector("#google-sign-in")
-  signInEl.addEventListener('click', () => signInWithProvider('google.com'))
 }
+
+const signInEl = document.body.querySelector("#google-sign-in")
+signInEl.addEventListener('click', () => signInWithProvider('google.com'))
